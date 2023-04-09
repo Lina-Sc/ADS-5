b7abad7987e0caa3ab312d10fdfa930973675fba
Lina-Sc <125125270+Lina-Sc@users.noreply.github.com>
2023-04-09 16:16:11 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,18 +4,18 @@
 
 template<typename T, int size>
 class TStack {
-private:
+
+ private:
     T arr[100];
     int top;
 
-public:
+ public:
     TStack() :top(-1) {}
 
     void push(T value) {
         if (isFull()) {
             throw std::string("Stack is full");
-        }
-        else {
+        } else {
             arr[++top] = value;
         }
     }
@@ -23,8 +23,7 @@ public:
     const T& pop() {
         if (isEmpty()) {
             throw std::string("Stack is empty");
-        }
-        else {
+        } else {
             return arr[top--];
         }
     }
